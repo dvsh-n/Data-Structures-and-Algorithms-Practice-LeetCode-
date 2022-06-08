1e3ce3cde5f273f6be4d4fc3a1db958922a52be2
Devesh Nath <deveshnath101@gmail.com>
2022-06-07 22:25:57 -0700

    Adding more projects

--- Arrays/MergeSortedArrays.py
+++ Arrays/MergeSortedArrays.py
@@ -1,32 +1,22 @@
-from logging import PlaceHolder
-
-
-def merge_arrays(arr1, arr2):
+def merge_arrays(nums1, nums2):
 
     merged_arr = []
     i = 0
     j = 0
-    total_len = len(arr1) + len(arr2)
+    total_len = len(nums1) + len(nums2)
 
     while len(merged_arr) != total_len:
-        if i == len(arr1):
-            num1 = num2 + 1
-        else:
-            num1 = arr1[i]
+        if i != len(nums1) or j != len(nums2):
+            temp1 = nums1[i]
+            temp2 = nums2[j]
 
-        if j == len(arr2):
-            num2 = num1 + 1
-        else:
-            num2 = arr2[j]
+            if temp1 <= temp2:
+                merged_arr.append(temp1)
+                i = i + 1
+            else:
+                merged_arr.append(temp2)
+                j = j + 1
+        
 
-        if num1 <= num2:
-            merged_arr.append(num1)
-            i = i + 1
-        else:
-            merged_arr.append(num2)
-            j = j + 1
 
-    return merged_arr
-
-print(merge_arrays([1,5,9,20,30,34], [4,7,10,30,50,80]))
 
