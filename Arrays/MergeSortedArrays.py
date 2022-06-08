ba692c658d21d48e50586d96e70ec27d3169cf7d
Devesh Nath <deveshnath101@gmail.com>
2022-06-08 14:47:17 -0700

    Completed merge sorted arrays

--- Arrays/MergeSortedArrays.py
+++ Arrays/MergeSortedArrays.py
@@ -6,7 +6,8 @@ def merge_arrays(nums1, nums2):
     total_len = len(nums1) + len(nums2)
 
     while len(merged_arr) != total_len:
-        if i != len(nums1) or j != len(nums2):
+
+        if i < len(nums1) and j < len(nums2):
             temp1 = nums1[i]
             temp2 = nums2[j]
 
@@ -16,7 +17,16 @@ def merge_arrays(nums1, nums2):
             else:
                 merged_arr.append(temp2)
                 j = j + 1
-        
 
+        else:
+            if i == len(nums1):
+                merged_arr = merged_arr + nums2[j:]
+            else:
+                merged_arr = merged_arr + nums1[i:]
+            return merged_arr
+
+    return merged_arr
+        
+print(merge_arrays([1,4,9,25,30,41],[5,7,10,20,25,30]))
 
 
