6010b0633ac5fa317c29ee349615f227bf6140f0
Devesh Nath <deveshnath101@gmail.com>
2022-06-09 21:46:15 -0700

    Solve the errors in next session

--- Linked Lists/LinkedList.py
+++ Linked Lists/LinkedList.py
@@ -1,5 +1,36 @@
-class linked_list():
+class node():
     def __init__(self, value):
         self.value = value
         self.next = None
-        
+
+class linked_list():
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.length = 0
+
+    def print_all(self):
+        temp = self.head
+        print(str(temp.value), end = ' -> ')
+        while temp.next != None:
+            print(str(temp.value), end = ' -> ')
+            temp = temp.next
+        print('\n')
+
+    def add_node(self, value):
+        if self.length == 0:
+            self.head = node(value)
+            self.tail = self.head
+        else:
+            new_node = self.head
+            while new_node.next != None:
+                new_node = new_node.next
+            new_node.next = node(value)
+            self.tail = new_node.next
+        self.length = self.length + 1
+
+new_list = linked_list()
+new_list.add_node(9)
+new_list.add_node(10)
+new_list.add_node(20)
+new_list.print_all()
