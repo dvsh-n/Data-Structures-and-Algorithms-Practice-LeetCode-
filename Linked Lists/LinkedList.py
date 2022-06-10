b61ac2ab0e1b05e7798113095fca0d2064c33181
Devesh Nath <deveshnath101@gmail.com>
2022-06-10 11:36:03 -0700

    Completed traverse_to_index Method

--- Linked Lists/LinkedList.py
+++ Linked Lists/LinkedList.py
@@ -28,12 +28,23 @@ class linked_list():
                 last = last.next
             last.next = new_node
             self.tail = new_node
-        self.length = self.length + 1
+        self.length += 1
 
     def append_many(self, *args):
         for i in args:
             self.append(i)
 
+    def traverse(self, index):
+        temp = self.head
+        i = 0
+        while i != index:
+            temp = temp.next
+            i += 1
+        return temp.value
+        
+
+
 new_list = linked_list()
-new_list.append_many(9,10,20,30)
+new_list.append_many(9,10,20,30,22)
+print(new_list.traverse(0))
 new_list.print_all()
