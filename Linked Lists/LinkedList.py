221594fe474bf5d5f7fb6b6dfc5e6e074a3500ea
Devesh Nath <deveshnath101@gmail.com>
2022-06-10 20:46:25 -0700

    Made changes to traverse_to_index to account for negative indexes

--- Linked Lists/LinkedList.py
+++ Linked Lists/LinkedList.py
@@ -33,13 +33,19 @@ class linked_list():
     def append_many(self, *args):
         for i in args:
             self.append(i)
+        self.length += len(args)
 
     def traverse_to_index(self, index):
         temp = self.head
         i = 0
-        while i != index:
-            temp = temp.next
-            i += 1
+        if index >= 0:
+            while i != index:
+                temp = temp.next
+                i += 1
+        else:
+            while i != (self.length + index):
+                temp = temp.next
+                i += 1
         return temp
 
     def insert(self, value, index):
@@ -48,13 +54,16 @@ class linked_list():
         new_Node = node(value)
         new_Node.next = next_Node
         prev_Node.next = new_Node
-        self.length += 1
+        self.length += 1        
+
+    # def reverse(self):
+    #     for i in range(int(self.length/2)):
 
 
-        
 
 
 new_list = linked_list()
 new_list.append_many(9,10,20,30,22)
 new_list.insert(35,2)
+print(new_list.traverse_to_index(-3).value)
 new_list.print_all()
