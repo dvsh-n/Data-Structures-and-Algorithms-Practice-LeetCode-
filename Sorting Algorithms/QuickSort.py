0dff172d11663775e1deeb84dbc5d87477674461
Devesh Nath <deveshnath101@gmail.com>
2022-06-26 19:34:21 -0700

    Added a base case

--- Sorting Algorithms/QuickSort.py
+++ Sorting Algorithms/QuickSort.py
@@ -3,5 +3,16 @@ import random
 nums = [26,79,13,65,24,46,876,43,214,87,36,98]
 
 def quick_sort(nums):
-    pivot_idx = random.randint(0, len(nums)-1)
-    pivot = nums[pivot_idx]
+    sorted  = True
+    for i in range(len(nums) - 1):
+        if nums[i] > nums[i+1]:
+            sorted = False
+
+    if sorted:
+        return nums
+    else:
+        pivot_idx = random.randint(0, len(nums)-1)
+        pivot = nums[pivot_idx]
+        i = 0
+        while i < pivot_idx:
+            pass
