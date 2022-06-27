d0e92e28b3c2cdb3ebf9b833538d554f38931056
Devesh Nath <deveshnath101@gmail.com>
2022-06-26 19:53:19 -0700

    Checkpoint

--- Sorting Algorithms/QuickSort.py
+++ Sorting Algorithms/QuickSort.py
@@ -1,6 +1,7 @@
 import random
 
-nums = [26,79,13,65,24,46,876,43,214,87,36,98]
+nums = [3,7,8,5,2,1,9,5,4]
+n = [5,8,9,5,7]
 
 def quick_sort(nums):
     sorted  = True
@@ -11,8 +12,19 @@ def quick_sort(nums):
     if sorted:
         return nums
     else:
-        pivot_idx = random.randint(0, len(nums)-1)
+        # pivot_idx = random.randint(0, len(nums)-1)
+        pivot_idx = len(nums) - 1
         pivot = nums[pivot_idx]
         i = 0
         while i < pivot_idx:
-            pass
+            if nums[i] > pivot:
+                temp = nums[i]
+                nums[i] = nums[pivot_idx - 1]
+                nums[pivot_idx - 1] = pivot
+                nums[pivot_idx] = temp
+                pivot_idx -= 1
+            if nums[i] < pivot:
+                i += 1
+        return (nums), pivot_idx
+
+print(quick_sort(nums))
