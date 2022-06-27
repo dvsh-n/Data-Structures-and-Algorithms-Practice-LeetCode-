1bff63ca867a75cca892cbb69cff94bd645fd9b7
Devesh Nath <deveshnath101@gmail.com>
2022-06-26 18:44:49 -0700

    Comparator function complete

---
+++ Sorting Algorithms/MergeSort.py
@@ -0,0 +1,30 @@
+nums = [26,79,13,65,24,46,876,43,214,87,36,98]
+
+def merge_sort(nums):
+    if len(nums) == 1:
+        return nums
+    center = nums//2
+    left = nums[:center]
+    right = nums[center:]
+
+def comparator(left, right):
+    result = []
+    i,j = 0,0
+
+    while i != len(left) and j != len(right):
+        if left[i] < right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+
+    if i == len(left):
+        result += right[j:]
+    else:
+        result += left[i:]
+    
+    return result
+
+print(comparator([1,2,5,7,8], [1,2,6,7,8]))
+    
