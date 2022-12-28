b65fc37af9029ac3cc7ccf09b1f39245637990df
Devesh Nath <deveshnath101@gmail.com>
2022-12-28 17:03:27 +0530

    Fixed an error
    I was iterating over data instead of range len data.

--- 2279_Medium.py
+++ 2279_Medium.py
@@ -1,36 +1,39 @@
 def maximumBags(capacity, rocks, additionalRocks):
-        result = 0
-        diff = []
-        max = 0
-        for i in range(len(capacity)):
-            diff_i = capacity[i] - rocks[i]
-            if diff_i > max:
-                max = diff_i
-            diff.append(diff_i)
-        print(max)
+    result = 0
+    diff = []
+    max = 0
+    for i in range(len(capacity)):
+        diff_i = capacity[i] - rocks[i]
+        if diff_i > max:
+            max = diff_i
+        diff.append(diff_i)
+    print(max)
 
-        data = [0 for j in range(max+1)]
-        print(data)
-        print(diff)
+    data = [0 for j in range(max+1)]
+    print(data)
+    print(diff)
 
-        for i in diff:
-            data[i] += 1
-        print(data)
+    for i in diff:
+        data[i] += 1
+    print(data)
 
-        # for i in range(len(capacity)):
-        #     diff.append(capacity[i] - rocks[i])
-        #     if diff[i] == 0:
-        #         result += 1
-        # for j in range(1,len(additionalRocks)):
-            #     if j < additionalRocks:
-            #         additionalRocks -= j
-            #         result += 1
-            #     else:
-            #         if additionalRocks != 0:
-            #             additionalRocks = 0
-            #             result += 1
-        # return result
+    for i in range(len(data)):
+        if  i == 0:
+            result += data[i]
+        else:
+            total = data[i] * i # no. of bags where i rocks can be filled * i
+            if total <= additionalRocks:
+                additionalRocks -= total
+                result += data[i]
+            elif total > additionalRocks:
+                remainder = total % additionalRocks
+                if remainder == 0:
+                    result += data[i]
+                else:
+                    result += data[i] - 1
+                additionalRocks = 0
+            break
 
-# maximumBags([2,3,4,5], [1,2,4,4], 2)
-# [0,0,0,0,2] 5
-print(8%5)
+    return result
+
+print(maximumBags([2,3,4,5], [1,2,4,4], 2))
